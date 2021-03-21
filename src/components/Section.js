d7f3c4003b1964d312d1ee0ab5294f974eca6e2d
Евгений Гейер <evgeny.geyer@ya.ru>
2021-03-21 11:18:19 +0300

    создаен класс для генерации секции

---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+export default class Section {
+  constructor({ renderer }, containerSelector) {
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems(items) {
+    items.forEach((item) => this._renderer(item));
+  }
+
+  appendItem(element) {
+    this._container.append(element);
+  }
+
+  prependItem(element) {
+    this._container.prepend(element);
+  }
+}
