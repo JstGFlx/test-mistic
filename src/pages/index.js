41ea3006c8252170b33fa5208adea48bffc15890
Evgeny Geyer <evgeny.geyer@ya.ru>
2021-03-22 15:19:42 +0300

    настроена сборка webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { btnsPredict1 } from '../utils/constants.js';
 import { renderPredict2 } from '../components/renderPredict2.js';
+import './index.css';
 
 btnsPredict1.forEach((btn) => btn.addEventListener('click', renderPredict2));
